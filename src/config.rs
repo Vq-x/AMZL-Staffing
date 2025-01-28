368b5588e545528a757975efeabc91f77864565f
Vq-x <74271294+Vq-x@users.noreply.github.com>
2025-01-28 17:05:10 -0500

    config file added

---
+++ src/config.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
+    pub target_pph: i32,
+    pub total_hours: i32,
+    // Add other configuration fields as needed
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            target_pph: 250,
+            total_hours: 6,
+        }
+    }
+}
